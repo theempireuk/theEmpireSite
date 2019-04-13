59e5aa262ba82c93af958ed1d0e55da631b0c093
Seb Ringrose <sebringrose@gmail.com>
2019-04-14 01:30:44 +0200

    initial stellar styling done

---
+++ scripts/landing.js
@@ -0,0 +1,44 @@
+// term cyler and button loader for landing splash
+terms = [
+  "lightspeed ",
+  "data-driven ",
+  "stellar ",
+  "reactive ",
+  "really good static "
+]
+i = 0
+
+var term = document.getElementById("term")
+var continueButton = document.getElementById("continue")
+
+var landingInterval = setInterval( function() {
+  if (i < terms.length) {
+    term.style.opacity = 0
+    termTransition(i)
+    i++
+  } else {
+    fadeButton()
+    clearInterval(landingInterval)
+  }
+}, 1500)
+
+// functions
+function termTransition(i) {
+  let termOpacity = 0;
+  let intervalTimer = 0;
+  term.textContent = terms[i]
+  var fadeInterval = setInterval( function() {
+    if (termOpacity >= 1) clearInterval(fadeInterval)
+    termOpacity += 0.05
+    term.style.opacity = termOpacity
+  }, 50)
+}
+
+function fadeButton() {
+  let buttonOpacity = 0;
+  var fadeInterval = setInterval( function() {
+    if (buttonOpacity >= 1) clearInterval(fadeInterval)
+    buttonOpacity += 0.05
+    continueButton.style.opacity = buttonOpacity
+  }, 50)
+}
